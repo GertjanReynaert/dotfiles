e96ad0f43cb237018c0b3369c5d349cc6c7e3459
Gertjan Reynaert <gertjan.reynaert@gmail.com>
2017-08-12 13:50:10 +0200

    Change prompt from displaying full path to just the current folder

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -112,7 +112,7 @@ prompt_pure_preprompt_render() {
   local -a preprompt_parts
 
   # Set the path.
-  preprompt_parts+=('%F{blue}%~%f')
+  preprompt_parts+=('%F{blue}%c%f')
 
   # Add git branch and dirty status info.
   typeset -gA prompt_pure_vcs_info
