baaabd0bed2d48853b36388239c50b75a1440c00
Gertjan Reynaert <gertjan.reynaert@gmail.com>
2021-03-30 16:58:17 +0200

    Update node to v14

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,4 +17,4 @@ set pure_color_green (set_color magenta)
 set pure_color_normal (set_color white)
 set pure_color_red (set_color red)
 set pure_color_yellow (set_color yellow)
-set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/node@14/bin" $fish_user_paths
