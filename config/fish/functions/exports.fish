ebdc511cb02af37d96e7f12ed4784babf576e66e
Gertjan Reynaert <gertjan.reynaert@gmail.com>
2021-04-01 13:19:39 +0200

    Correctly set java_home

--- config/fish/functions/exports.fish
+++ config/fish/functions/exports.fish
@@ -15,6 +15,8 @@ set -xg EDITOR "vim"
 set -xg TERM "xterm-256color"
 set -xg ANDROID_HOME $HOME/Library/Android/sdk
 
+set -x JAVA_HOME (/usr/libexec/java_home) # https://superuser.com/questions/1401673/how-to-set-a-specific-version-of-java-as-java-home-in-fish-in-macos
+
 # Paths
 set PATH /usr/local/bin $PATH
 # set PATH /usr/local/sbin $PATH
