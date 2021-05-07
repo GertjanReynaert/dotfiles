fa350a4bcdeb8b3028ada376230676af63685045
Gertjan Reynaert <gertjan.reynaert@gmail.com>
2021-05-07 12:47:35 +0200

    :fire: Remove rbenv because asdf, add python path

--- config/fish/functions/exports.fish
+++ config/fish/functions/exports.fish
@@ -7,13 +7,13 @@ set -x LC_COLLATE en_US.UTF-8
 set -x LC_MONETARY en_US.UTF-8
 set -x LC_MESSAGES en_US.UTF-8
 
-
 # Applications
 set -xg PAGER "less"
 set -xg MANPAGER "$PAGER"
 set -xg EDITOR "vim"
 set -xg TERM "xterm-256color"
 set -xg ANDROID_HOME $HOME/Library/Android/sdk
+set -xg ANDROID_SDK_ROOT $HOME/Library/Android/sdk
 
 set -x JAVA_HOME (/usr/libexec/java_home) # https://superuser.com/questions/1401673/how-to-set-a-specific-version-of-java-as-java-home-in-fish-in-macos
 
@@ -23,18 +23,13 @@ set PATH /usr/local/bin $PATH
 set PATH $HOME/.bin $PATH
 # set PATH $PATH $HOME/bin
 
-# Path rbenv
-# set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
+set PATH $HOME/Library/Python/3.9/bin $PATH
 
 # Path android
 set PATH $PATH $ANDROID_HOME/emulator
 set PATH $PATH $ANDROID_HOME/tools
 set PATH $PATH $ANDROID_HOME/tools/bin
 set PATH $PATH $ANDROID_HOME/platform-tools
-
-# Rbenv
-status --is-interactive; and . (rbenv init -|psub)
 
 # NVIM
 export NVIM_TUI_ENABLE_TRUE_COLOR 0
