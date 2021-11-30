8aff2fe0be0de65241b7742a6a6daacfabe25363
Gertjan Reynaert <gertjan.reynaert@gmail.com>
2021-11-30 16:58:54 +0100

    ✨ Extend aliases

--- config/fish/functions/aliases.fish
+++ config/fish/functions/aliases.fish
@@ -1,17 +1,27 @@
-alias g "git"
-
-alias e "vim"
+alias ll "ls -al"
+alias ln "ln -v"
+alias mkdir "mkdir -p"
+alias e "$EDITOR"
+alias v "$VISUAL"
 
 alias claer "clear"
 alias clera "clear"
+
+alias g "git"
+
+alias b "bundle"
+alias be "bundle exec"
 
 alias usage "vtop"
 alias cpu "vtop"
 alias monitor "vtop"
 alias activity "vtop"
 
-alias code "cd ~/Code"
-alias hoby "cd ~/Code/gertjanreynaert"
+alias back "cd -"
+alias dev "cd ~/Code"
+alias hobby "cd ~/Code/gertjanreynaert"
+alias personal "hobby"
 alias work "cd ~/Code/ITP"
 
-alias be "bundle exec"
+alias path "echo $PATH | tr -s ':' '\n'"
+
