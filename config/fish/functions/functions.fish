5fe2b3bec207c545700467810a8a5f7d3bbd3fd9
Gertjan Reynaert <gertjan.reynaert@gmail.com>
2022-07-08 15:29:37 +0200

    🐛 Temp fix illegal -p option error

--- config/fish/functions/functions.fish
+++ config/fish/functions/functions.fish
@@ -1,8 +1,8 @@
-function mkdir
-  # command is needed to avoid having errors of calling ourselves
-  command mkdir -p "$argv"
-  cd "$argv[1]"
-end
+# function mkdir
+#   # command is needed to avoid having errors of calling ourselves
+#   command mkdir -p "$argv"
+#   cd "$argv[2]"
+# end
 
 function addDockSpace
   defaults write com.apple.dock persistent-apps -array-add "{"tile-type"="spacer-tile";}"; and killall Dock
