674f3cbb1802e64fc1f844119bf92c11c7521468
Gertjan Reynaert <gertjan.reynaert@gmail.com>
2023-07-06 15:28:55 +0200

    :sparkles: Add some helper methods for starting simulators

--- config/fish/functions/functions.fish
+++ config/fish/functions/functions.fish
@@ -26,3 +26,30 @@ end
 function find_cyclical_dependencies
   npx madge --ts-config tsconfig.json --circular --extensions ts,tsx src
 end
+
+function restart_adb
+  adb kill-server
+  adb start-server
+end
+
+function run_all_device_sizes
+  # iPhone SE
+  npm run ios:debug -- --udid="D1BD4DF5-6AAC-442B-8A3F-98C9462D5A82"
+
+  # iPhone 14 (Configured with larger text size)
+  npm run ios:debug -- --udid="AD5DD695-E44F-43B9-A73C-D168A94D25CB"
+
+  # iPhone 14 Pro
+  npm run ios:debug -- --udid="0144ED7F-2233-43AA-9165-48D887DB33CE"
+
+  # iPhone 14 Plus
+  npm run ios:debug -- --udid="8F3624D9-1C3E-46A7-94D6-9CD7215492AD"
+
+  # android
+  npm run android:debug
+end
+
+function run_both
+  npm run ios:debug
+  npm run android:debug
+end
