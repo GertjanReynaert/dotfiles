84ec8e7baba74a0edbb0bd934f80144851968829
Gertjan Reynaert <gertjan.reynaert@gmail.com>
2024-04-08 15:27:54 +0200

    🔥 Cleanup of unused fish alias

--- config/fish/functions/functions.fish
+++ config/fish/functions/functions.fish
@@ -1,9 +1,3 @@
-# function mkdir
-#   # command is needed to avoid having errors of calling ourselves
-#   command mkdir -p "$argv"
-#   cd "$argv[2]"
-# end
-
 function add_dock_space
   defaults write com.apple.dock persistent-apps -array-add "{"tile-type"="spacer-tile";}"; and killall Dock
 end
