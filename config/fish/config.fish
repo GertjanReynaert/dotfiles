07c11f45f0eee173e547ccc327bacf3412fc2cb6
Gertjan Reynaert <gertjan.reynaert@gmail.com>
2024-04-22 21:01:36 +0200

    🚚 Move all pure setup to a separate sourced file

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,16 +8,4 @@ source "$HOME/.config/fish/functions/functions.fish"
 source (brew --prefix asdf)/libexec/asdf.fish
 source "$HOME/.asdf/plugins/java/set-java-home.fish"
 
-# THEME PURE
-# Use this installer: https://github.com/rafaelrinaldi/pure#manually
-source /Users/gertjanreynaert/.config/fish/functions/theme-pure/conf.d/pure.fish
-set fish_function_path /Users/gertjanreynaert/.config/fish/functions/theme-pure/functions/ $fish_function_path
-
-set pure_color_blue (set_color blue)
-set pure_color_cyan (set_color cyan)
-set pure_color_gray (set_color "686868")
-set pure_color_green (set_color magenta)
-set pure_color_normal (set_color white)
-set pure_color_red (set_color red)
-set pure_color_yellow (set_color yellow)
-
+source "$HOME/.config/fish/functions/pure.fish"
---
+++ config/fish/functions/pure.fish
@@ -0,0 +1,12 @@
+# THEME PURE
+# Use this installer: https://github.com/rafaelrinaldi/pure#manually
+source /Users/gertjanreynaert/.config/fish/functions/theme-pure/conf.d/pure.fish
+set fish_function_path /Users/gertjanreynaert/.config/fish/functions/theme-pure/functions/ $fish_function_path
+
+set pure_color_blue (set_color blue)
+set pure_color_cyan (set_color cyan)
+set pure_color_gray (set_color "686868")
+set pure_color_green (set_color magenta)
+set pure_color_normal (set_color white)
+set pure_color_red (set_color red)
+set pure_color_yellow (set_color yellow)
